4820f58008b51b29e1b3cb2f4e3a3f26be35ea6c
Maksym Zyza <Maksym.Zyza@techiosoft.com>
2026-01-26 09:23:21 +0200

    feat: add vpn module

--- lesson-5/main.tf
+++ lesson-5/main.tf
@@ -9,15 +9,15 @@ module "s3_backend" {
   table_name  = "terraform-locks"
 }
 
-# # Підключаємо модуль VPC
-# module "vpc" {
-#   source             = "./modules/vpc"
-#   vpc_cidr_block     = "10.0.0.0/16"
-#   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-#   private_subnets    = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-#   availability_zones = ["us-west-2a", "us-west-2b", "us-west-2c"]
-#   vpc_name           = "lesson-5-vpc"
-# }
+# Підключаємо модуль VPC
+module "vpc" {
+  source              = "./modules/vpc"                                       # Шлях до модуля VPC
+  vpc_cidr_block      = "10.0.0.0/16"                                         # CIDR блок для VPC
+  public_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]         # Публічні підмережі
+  private_subnets     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]         # Приватні підмережі
+  availability_zones  = ["us-west-2a", "us-west-2b", "us-west-2c"]            # Зони доступності
+  vpc_name            = "vpc"                                                 # Ім'я VPC
+}
 
 # # Підключаємо модуль ECR
 # module "ecr" {
