39f64767e779ebb2e39da5d32487bca0ea569244
Maksym Zyza <Maksym.Zyza@techiosoft.com>
2026-01-26 19:42:40 +0200

    chore: add vpc outputs

--- lesson-6/outputs.tf
+++ lesson-6/outputs.tf
@@ -1,4 +1,4 @@
-# Вивід S3 бакета та DynamoDB таблиці для бекенду
+# ===== Вивід S3 бакета та DynamoDB таблиці для бекенду
 output "s3_bucket_name" {
   description = "Назва S3-бакета для стейтів"
   value       = module.s3_backend.s3_bucket_name
@@ -9,25 +9,35 @@ output "dynamodb_table_name" {
   value       = module.s3_backend.dynamodb_table_name
 }
 
-# Вивід VPC
+# ===== Вивід VPC
 output "vpc_id" {
   description = "ID створеної VPC"
   value       = module.vpc.vpc_id
 }
 
-# Вивід VPC підмереж
 output "public_subnets" {
   description = "ID публічних підмереж"
   value       = module.vpc.public_subnets
 }
 
-# Вивід ECR 
+
+output "private_subnets" {
+  description = "List of private subnet IDs"
+  value       = module.vpc.private_subnets
+}
+
+output "internet_gateway_id" {
+  description = "ID of the Internet Gateway"
+  value       = module.vpc.internet_gateway_id
+}
+
+# ===== Вивід ECR 
 output "ecr_repository_url" {
   description = "URL ECR репозиторію для Docker push"
   value       = module.ecr.repository_url
 }
 
-# Вивід EKS
+# ===== Вивід EKS
 output "eks_cluster_endpoint" {
   description = "EKS API endpoint для підключення до кластера"
   value       = module.eks.cluster_endpoint
