90272f13b904fcaf78b52e3a7681cf6a4b93e561
Maksym Zyza <Maksym.Zyza@techiosoft.com>
2026-02-06 12:41:18 +0200

    feat: Add Jenkins CI configuration

--- lesson-8-9/main.tf
+++ lesson-8-9/main.tf
@@ -48,12 +48,26 @@ provider "helm" {
   }
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    command     = "aws"
+  }
+}
+
 module "jenkins" {
-  source       = "./modules/jenkins"
-  cluster_name = module.eks.cluster_name
+  source              = "./modules/jenkins"
+  cluster_name        = module.eks.cluster_name
+  oidc_provider_arn   = module.eks.oidc_provider_arn
+  oidc_provider_url   = module.eks.oidc_provider_url
 
   providers = {
-    helm = helm
+    helm       = helm
+    kubernetes = kubernetes
+    aws        = aws
   }
 }
 
